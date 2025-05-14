cb52627e7aafe0496f1f882a303acfb59be7ac09
Muhamad Aenun Zidane <muhammad.aenun@stellargeloratech.com>
2025-05-14 17:32:08 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
